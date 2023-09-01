4fdfae20653a1aa4552a515cb03f7b0a829ff0c3
Gitatron Maximus <blockhead.onchain@gmail.com>
2023-09-01 17:17:27 -0400

    20. Deposits pt 1

--- src/components/App.js
+++ src/components/App.js
@@ -7,11 +7,13 @@ import {
   loadNetwork, 
   loadAccount, 
   loadTokens,
-  loadExchange 
+  loadExchange,
+  subscribeToEvents 
 } from '../store/interactions';
 
 import Navbar from './Navbar'
 import Markets from './Markets'
+import Balance from './Balance'
 
 
 function App() {
@@ -37,7 +39,10 @@ function App() {
     await loadTokens(provider, [Dapp.address, mETH.address], dispatch)
 
     const exchangeConfig = config[chainId].exchange
-    await loadExchange(provider, exchangeConfig.address, dispatch)
+    const exchange = await loadExchange(provider, exchangeConfig.address, dispatch)
+  
+    subscribeToEvents(exchange, dispatch)
+
   }
 
   useEffect(()  => {
@@ -53,6 +58,8 @@ function App() {
         <section className='exchange__section--left grid'>
 
           <Markets />
+
+          <Balance />
 
           {/* Balance */}
 
